a00b9e6c5ef1d51db510569098055ce2eda95b16
徐涛焘 <xutaotao1108@outlook.com>
2020-03-29 14:11:21 +0800

    修改getValueByPath

--- vue.js
+++ vue.js
@@ -1,56 +1,72 @@
 function LikeVue (options) {
   this._data = options.data;
-  this._el = options.el
+  this._el = options.el;
   this.$el = this._templateDOM = document.querySelector(this._el);
   this._parent = this._templateDOM.parentNode;
-  this.render()
+  this.render();
 }
 
-// 递归遍历节点替换值
+// traverse node replace valuse
 function compiler (template, data) {
-  // 正则匹配{{}}
+  // Regular match{{}}
   const bracketReg = /\{\{(.+?)\}\}/g;
-  let childNodes  = template.childNodes; 
-  // 循环节点
+  let childNodes  = template.childNodes;
+  // loop nodes
   for(let i = 0; i < childNodes.length; i++){
     let  type = childNodes[i].nodeType;
-    // 文本节点
+    // text  node
     if(type === 3){
       let txt = childNodes[i].nodeValue;
 
       txt = txt.replace(bracketReg, function (_, g) {
-        let key = g.trim();
-        let value = data[key];
+        let path = g.trim();
+        let value = getValueByPath(data, path);
         return value;
-      })
+      });
 
-      childNodes[i].nodeValue = txt
-    } else if(type === 1) { // 元素
+      childNodes[i].nodeValue = txt;
+    } else if(type === 1) { // element
       compiler(childNodes[i], data);
     }
   }
 }
 
+function getValueByPath (obj, path) {
+  let paths = path.split('.');
+  let res = obj;
+  let prop = paths.shift();
+  while (prop){
+    res = res[prop];
+    prop = paths.shift();
+  }
+  return res;
+}
+
+// function createGetValueByPath (path) {
+//   let paths = path.split('.');
+//   return function getValueByPath (obj) {
+//     let res = obj;
+//     let prop = paths.shift();
+//     while (prop){
+//       res = res[prop];
+//       prop = paths.shift();
+//     }
+//     return res;
+//   };
+// }
 LikeVue.prototype.render = function () {
   this.compiler();
-}
+};
 
 LikeVue.prototype.compiler = function () {
-  // 获得真正的节点
+  // get the real node
   let realHTMLDOM = this._templateDOM.cloneNode(true);
   compiler(realHTMLDOM,this._data);
-  this.update(realHTMLDOM)
-}
+  this.update(realHTMLDOM);
+};
 
 LikeVue.prototype.update = function (real) {
-  // 替换
-  this._parent.replaceChild(real,document.querySelector('#root'))
-}
+  // replace
+  this._parent.replaceChild(real,document.querySelector('#root'));
+};
 
-let app = new LikeVue({
-  el:'#root',
-  data: {
-    name:'Taotao',
-    message:'Hello'
-  }
-})
